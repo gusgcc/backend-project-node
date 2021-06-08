cabd5dbb52f88827708d4e67d2720c64042f9d84
Gustavo Gil Condezo Camayo <gustavocondezocamayo@gmail.com>
2021-06-07 23:08:41 -0500

    [TEST AUTO DEPLOY]

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const config = require("./server/config");
 //le pasamos el app de express() y nos devuelve un app configurado
 const app = config(express());
 
+
 app.listen(app.get("port"), () => {
     console.log("servidor en : ", app.get("port"))
 })
