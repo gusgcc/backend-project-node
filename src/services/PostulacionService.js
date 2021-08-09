df6ad297cf623e2d103069ede5e819ceb106cc7c
Ronald <ronald@latamready.com>
2021-08-09 18:33:03 -0500

    Retornando Publicaciones

--- src/services/PostulacionService.js
+++ src/services/PostulacionService.js
@@ -1,6 +1,7 @@
 const postulacionService = {}
 var mongoose = require('mongoose')
 const Postulacion = require("../models/modelosBD/Postulacion")
+const PublicacionService= require("../services/PublicacionService")
 
 postulacionService.crearPostulacion = async (crearPostulacionModel) => {
     const newPostulacion = new Postulacion({
@@ -18,15 +19,20 @@ postulacionService.crearPostulacion = async (crearPostulacionModel) => {
 postulacionService.traerPostulaciones= async(idPostulante)=>{
     try {
         let postulaciones = await Postulacion.find({idPostulante:idPostulante});
-        let idPublicaciones=[];
+        let publicaciones=[];
         //console.log(postulaciones);
         for (i in postulaciones){
-            idPublicaciones.push(postulaciones[i].idPublicacion);
-            console.log(postulaciones[i].idPublicacion);
+            //console.log(postulaciones[i].idPublicacion);
+            //idPublicaciones.push(postulaciones[i].idPublicacion);
+            let publicacion=await PublicacionService.traerPublicacionId(postulaciones[i].idPublicacion);
+            if(publicacion){
+                publicaciones.push(publicacion)
+            }
+            console.log(publicacion);
         }
         
         //return postulaciones;
-        return idPublicaciones;
+        return publicaciones;
     } catch (error) {
         console.log(error);
         return null ;
